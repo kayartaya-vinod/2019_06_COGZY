c914ecf25d641f91aaad48dd782e6842107f693b
kayartaya-vinod <vinod@vinod.co>
2019-07-11 21:08:40 +0530

    🇮🇳

---
+++ EclipseWorkspace/06 Data Structures/src/com/cogzy/programs/P03_TestingBinarySearchTree.java
@@ -0,0 +1,41 @@
+package com.cogzy.programs;
+
+import com.cogzy.utils.BinarySearchTree;
+
+public class P03_TestingBinarySearchTree {
+	
+	public static void main(String[] args) {
+		BinarySearchTree<String> tree = new BinarySearchTree<String>();
+		tree.add("Vinod");
+		tree.add("ajay");
+		tree.add("Anil");
+		tree.add("Vishal");
+		tree.add("Vinay");
+		tree.add("Shyam");
+		tree.add("Suresh");
+		tree.add("vinod");
+		tree.add("Ramesh");
+		
+		tree.print();
+	}
+	
+	public static void main_1(String[] args) {
+		
+		BinarySearchTree<Integer> tree = new BinarySearchTree<Integer>();
+
+		tree.add(51);
+		tree.add(21);
+		tree.add(7);
+		tree.add(5);
+		tree.add(51);
+		tree.add(75);
+		tree.add(56);
+		tree.add(53);
+		tree.add(65);
+		tree.add(51);
+		tree.add(10);
+		
+		tree.print();
+	}
+
+}
---
+++ EclipseWorkspace/06 Data Structures/src/com/cogzy/utils/BinarySearchTree.java
@@ -0,0 +1,106 @@
+package com.cogzy.utils;
+
+public class BinarySearchTree<T extends Comparable<T>> {
+
+	private Node<T> start;
+
+	public BinarySearchTree() {
+	}
+
+	public void add(T data) {
+		Node<T> newNode = new Node<T>(data);
+
+		add(newNode, start);
+
+	}
+
+	private void add(Node<T> newNode, Node<T> currNode) {
+
+		if (currNode == null) {
+			// happens only for the first element
+			start = newNode;
+			return;
+		}
+
+		// if the return value of data.compareTo(temp.data) < 0 --> data < temp.data
+		// if the return value of data.compareTo(temp.data) is 0 --> data == temp.data
+		// if the return value of data.compareTo(temp.data) > 0 --> data > temp.data
+		if (newNode.data.compareTo(currNode.data) < 0) {
+			if (currNode.left == null) {
+				currNode.left = newNode;
+			} else {
+				// navigate to left onlt if currNode.left != null
+				add(newNode, currNode.left);
+			}
+		} else { //  if (newNode.data.compareTo(currNode.data) > 0) {
+			if (currNode.right == null) {
+				currNode.right = newNode;
+			} else {
+				// navigate to right
+				add(newNode, currNode.right);
+			}
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return "[]";
+	}
+	
+	public void print() {
+		print(start);
+	}
+
+	private void print(Node<T> currNode) {
+		if (currNode == null) {
+			System.out.print("No data in the tree!\n");
+			return;
+		}
+
+		// navigate to left
+		if (currNode.left != null) {
+			print(currNode.left);
+		}
+		// access the element
+		System.out.print(currNode.data + ", ");
+		// navigate to right
+		if (currNode.right != null) {
+			print(currNode.right);
+		}
+	}
+
+	private class Node<T> {
+		private T data;
+		private Node<T> left;
+		private Node<T> right;
+
+		public Node(T data) {
+			this.data = data;
+		}
+
+		public void setData(T data) {
+			this.data = data;
+		}
+
+		public T getData() {
+			return data;
+		}
+
+		public void setLeft(Node<T> left) {
+			this.left = left;
+		}
+
+		public Node<T> getLeft() {
+			return left;
+		}
+
+		public void setRight(Node<T> right) {
+			this.right = right;
+		}
+
+		public Node<T> getRight() {
+			return right;
+		}
+	}
+
+}
